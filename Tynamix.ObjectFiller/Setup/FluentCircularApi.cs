28e853d1a5d3fb5294935cc5c6151350f6a0d5d6
dtaylor-530 <d.taylor530@gmail.com>
2023-02-03 12:40:45 +0000

    Removes Graph and Graph.WPF from solution

--- Tynamix.ObjectFiller/Setup/FluentCircularApi.cs
+++ Tynamix.ObjectFiller/Setup/FluentCircularApi.cs
@@ -7,6 +7,8 @@
 // </summary>
 // --------------------------------------------------------------------------------------------------------------------
 
+using System;
+
 namespace Tynamix.ObjectFiller
 {
     /// <summary>
@@ -55,6 +57,59 @@ namespace Tynamix.ObjectFiller
         public FluentFillerApi<TTargetObject> ThrowException(bool throwException = true)
         {
             this.setupManager.GetFor<TTargetObject>().ThrowExceptionOnCircularReference = throwException;
+
+            return this.callback;
+        }
+    }
+
+    /// <summary>
+    /// This API is for setup the behavior on circular dependencies
+    /// </summary>
+    /// <typeparam name="TTargetObject">Type which will be configured for the object filler</typeparam>
+    public class FluentCircularApi
+    {
+        /// <summary>
+        /// The callback function
+        /// </summary>
+        private readonly FluentFillerApi callback;
+
+        /// <summary>
+        /// The object filler setup manager.
+        /// </summary>
+        private readonly SetupManager setupManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FluentCircularApi{TTargetObject}"/> class.
+        /// </summary>
+        /// <param name="callback">
+        /// The callback.
+        /// </param>
+        /// <param name="setupManager">
+        /// The object filler setup manager.
+        /// </param>
+        internal FluentCircularApi(FluentFillerApi callback, SetupManager setupManager, Type type)
+        {
+            this.callback = callback;
+            this.setupManager = setupManager;
+            Type = type;
+        }
+
+        public Type Type { get; }
+
+        /// <summary>
+        /// Call this when you want to get an exception in case of a circular reference in your filled model.
+        /// By default the ObjectFiller recognizes circular references and stop filling them without throwing an exception.
+        /// When you want to get an explicit exception on circular reference call this method!
+        /// </summary>
+        /// <param name="throwException">
+        /// True (default) when you want to get exception on a circular reference
+        /// </param>
+        /// <returns>
+        /// The <see cref="FluentFillerApi{TTargetObject}"/>.
+        /// </returns>
+        public FluentFillerApi ThrowException(bool throwException = true)
+        {
+            this.setupManager.GetFor(Type).ThrowExceptionOnCircularReference = throwException;
 
             return this.callback;
         }
--- Tynamix.ObjectFiller/Setup/FluentFillerApi.cs
+++ Tynamix.ObjectFiller/Setup/FluentFillerApi.cs
@@ -11,7 +11,6 @@ namespace Tynamix.ObjectFiller
 {
     using System;
     using System.Collections.Generic;
-    using System.Diagnostics;
     using System.Linq.Expressions;
     using System.Reflection;
 
@@ -60,7 +59,6 @@ namespace Tynamix.ObjectFiller
             return new FluentTypeApi<TTargetObject, TTargetType>(this, this.setupManager);
         }
 
-
         /// <summary>
         /// Starts to configure the behavior of the ObjectFiller when a circular reference in your model occurs
         /// </summary>
@@ -91,7 +89,6 @@ namespace Tynamix.ObjectFiller
         public FluentPropertyApi<TTargetObject, TTargetType> OnProperty<TTargetType>(Expression<Func<TTargetObject, TTargetType>> property, params Expression<Func<TTargetObject, TTargetType>>[] additionalProperties)
         {
             var properties = new List<Expression<Func<TTargetObject, TTargetType>>> { property };
-
 
             if (additionalProperties.Length > 0)
             {
@@ -130,7 +127,6 @@ namespace Tynamix.ObjectFiller
             this.setupManager.GetFor<TTargetObject>().ListMaxCount = maxCount;
             return this;
         }
-
 
         /// <summary>
         /// Call this if the ObjectFiller should ignore all unknown types which can not filled automatically by the ObjectFiller.
@@ -242,6 +238,244 @@ namespace Tynamix.ObjectFiller
         /// </typeparam>
         /// <returns>
         /// <see cref="FluentFillerApi{TTargetObject}"/>
+        /// </returns>
+        public FluentFillerApi<TNewType> SetupFor<TNewType>(bool useDefaultSettings = false) where TNewType : class
+        {
+            this.setupManager.SetNewFor<TNewType>(useDefaultSettings);
+
+            return new FluentFillerApi<TNewType>(this.setupManager);
+        }
+    }
+
+    /// <summary>
+    /// Fluent API to configure the object filler.
+    /// </summary>
+    public class FluentFillerApi
+    {
+        /// <summary>
+        /// The object filler setup manager.
+        /// </summary>
+        private readonly SetupManager setupManager;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FluentFillerApi{TTargetObject}"/> class.
+        /// </summary>
+        /// <param name="setupManager">
+        /// The object filler setup manager.
+        /// </param>
+        /// <param name="type">Type which will be configured for the object filler</param>
+        internal FluentFillerApi(SetupManager setupManager, Type type)
+        {
+            this.setupManager = setupManager;
+            Type = type;
+        }
+
+        /// <summary>
+        /// Gets the current object filler setup
+        /// </summary>
+        public FillerSetup Result
+        {
+            get { return this.setupManager.FillerSetup; }
+        }
+
+        /// <summary>
+        /// Type which will be configured for the object filler
+        /// </summary>
+        public Type Type { get; }
+
+        /// <summary>
+        /// Start to configure a type for object filler. The follow up methods will be found in the <see cref="FluentTypeApi{TTargetObject,TTargetType}"/>
+        /// </summary>
+        /// <typeparam name="TTargetType">
+        /// Type which will be configured. For example string, integer, etc...
+        /// </typeparam>
+        /// <returns>
+        /// <see cref="FluentTypeApi{TTargetType}"/>
+        /// </returns>
+        public FluentTypeApi<TTargetType> OnType<TTargetType>(Type type)
+        {
+            return new FluentTypeApi<TTargetType>(this, this.setupManager, type);
+        }
+
+        /// <summary>
+        /// Starts to configure the behavior of the ObjectFiller when a circular reference in your model occurs
+        /// </summary>
+        /// <returns>
+        /// The <see cref="FluentCircularApi{TTargetObject}"/>.
+        /// </returns>
+        public FluentCircularApi OnCircularReference()
+        {
+            return new FluentCircularApi(this, this.setupManager, Type);
+        }
+
+        /// <summary>
+        /// Starts to configure a property of the <typeparamref name="TTargetObject"/> for object filler.
+        /// So you can setup a custom randomizer to a specific property within a class.
+        /// </summary>
+        /// <typeparam name="TTargetType">
+        /// The type of the target properties
+        /// </typeparam>
+        /// <param name="property">
+        /// The target property which will be configured
+        /// </param>
+        /// <param name="additionalProperties">
+        /// Some more properties which should be get the same configuration
+        /// </param>
+        /// <returns>
+        /// The <see cref="FluentPropertyApi{TProperty}"/>.
+        /// </returns>
+        public FluentPropertyApi<TProperty> OnProperty<TProperty>(Expression<Func<object, TProperty>> property, params Expression<Func<object, TProperty>>[] additionalProperties)
+        {
+            var properties = new List<Expression<Func<object, TProperty>>> { property };
+
+            if (additionalProperties.Length > 0)
+            {
+                properties.AddRange(additionalProperties);
+            }
+
+            var propInfos = new List<PropertyInfo>();
+            foreach (Expression<Func<object, TProperty>> propertyExp in properties)
+            {
+                var body = propertyExp.Body as MemberExpression;
+                if (body != null)
+                {
+                    var propertyInfo = body.Member as PropertyInfo;
+                    if (propertyInfo != null)
+                    {
+                        propInfos.Add(propertyInfo);
+                    }
+                }
+            }
+
+            return new FluentPropertyApi<TProperty>(propInfos, this, this.setupManager, Type);
+        }
+
+        /// <summary>
+        /// Setup the maximum item count for lists. The ObjectFiller will not generate more list items then this.
+        /// The default value is 25.
+        /// </summary>
+        /// <param name="maxCount">
+        /// Max items count in a list. Default: 25
+        /// </param>
+        /// <returns>
+        /// The <see cref="FluentFillerApi"/>.
+        /// </returns>
+        public FluentFillerApi ListItemCount(int maxCount)
+        {
+            this.setupManager.GetFor(Type).ListMaxCount = maxCount;
+            return this;
+        }
+
+        /// <summary>
+        /// Call this if the ObjectFiller should ignore all unknown types which can not filled automatically by the ObjectFiller.
+        /// When you not call this method, the ObjectFiller raises an exception when it is not possible to generate a random value for that type!
+        /// </summary>
+        /// <returns>The <see cref="FluentFillerApi"/></returns>
+        public FluentFillerApi IgnoreAllUnknownTypes()
+        {
+            this.setupManager.GetFor(Type).IgnoreAllUnknownTypes = true;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Call this if the ObjectFiller should ignore all properties of the base class. For example you have a class
+        /// 'Student' which derives from class 'Person' and the class Person has a property 'Name'. If you want to use ObjectFiller
+        /// to fill/create a student and you call this method, the name will be null because it is defined in the base class 'Person'
+        /// </summary>
+        /// <returns>The <see cref="FluentFillerApi{TTargetObject}"/></returns>
+        public FluentFillerApi IgnoreInheritance()
+        {
+            this.setupManager.GetFor(Type).IgnoreInheritance = true;
+
+            return this;
+        }
+
+        /// <summary>
+        /// Setup the minimum and maximum item count for lists. The ObjectFiller will not generate more or less list items then this limits.
+        /// The default value for <paramref name="minCount"/> is 1. The default value for <paramref name="maxCount"/> is 25.
+        /// </summary>
+        /// <param name="minCount">
+        /// Minimum item in a list. Default: 1
+        /// </param>
+        /// <param name="maxCount">
+        /// Maximum item in a list. Default: 25
+        /// </param>
+        /// <returns>
+        /// The <see cref="FluentFillerApi"/>
+        /// </returns>
+        public FluentFillerApi ListItemCount(int minCount, int maxCount)
+        {
+            this.setupManager.GetFor(Type).ListMinCount = minCount;
+            this.setupManager.GetFor(Type).ListMaxCount = maxCount;
+            return this;
+        }
+
+        /// <summary>
+        /// Setup the maximum count of keys in dictionaries. The ObjectFiller will not generate more list items then this.
+        /// The default value is 10.
+        /// </summary>
+        /// <param name="maxCount">
+        /// Max items count of keys in a dictionary. Default: 10
+        /// </param>
+        /// <returns>
+        /// The <see cref="FluentFillerApi"/>.
+        /// </returns>
+        public FluentFillerApi DictionaryItemCount(int maxCount)
+        {
+            this.setupManager.GetFor(Type).DictionaryKeyMaxCount = maxCount;
+            return this;
+        }
+
+        /// <summary>
+        /// Setup the minimum and maximum count of keys in dictionaries. The ObjectFiller will not generate more or less list items then this limits.
+        /// The default value for <paramref name="minCount"/> is 1. The default value for <paramref name="maxCount"/> is 25.
+        /// </summary>
+        /// <param name="minCount">
+        /// Minimum items count of keys in a dictionary. Default: 1
+        /// </param>
+        /// <param name="maxCount">
+        /// Max items count of keys in a dictionary. Default: 10
+        /// </param>
+        /// <returns>
+        /// The <see cref="FluentFillerApi{TTargetObject}"/>.
+        /// </returns>
+        public FluentFillerApi DictionaryItemCount(int minCount, int maxCount)
+        {
+            this.setupManager.GetFor(Type).DictionaryKeyMinCount = minCount;
+            this.setupManager.GetFor(Type).DictionaryKeyMaxCount = maxCount;
+            return this;
+        }
+
+        /// <summary>
+        /// Register a IInterfaceMocker which will Mock your interfaces which are not registered with the IFluentFillerApi{TTargetObject}.RegisterInterface{TInterface,TImplementation} method.
+        /// To use a Mocker like MOQ or RhinoMocks, it is necessary to implement the <see cref="IInterfaceMocker"/> for this mocking framework.
+        /// </summary>
+        /// <param name="mocker">Mocker which will be used to mock interfaces</param>
+        /// <returns>The <see cref="FluentFillerApi"/></returns>
+        public FluentFillerApi SetInterfaceMocker(IInterfaceMocker mocker)
+        {
+            if (this.setupManager.GetFor(Type).InterfaceMocker != null)
+            {
+                const string Message = "You can not set a interface mocker more than once!";
+                throw new ArgumentException(Message);
+            }
+
+            this.setupManager.GetFor(Type).InterfaceMocker = mocker;
+            return this;
+        }
+
+        /// <summary>
+        /// Create a setup for another type
+        /// </summary>
+        /// <param name="useDefaultSettings">
+        /// The use Default Settings.
+        /// </param>
+        /// <typeparam name="TNewType">
+        /// Type for which the setup will be created
+        /// </typeparam>
+        /// <returns>
+        /// <see cref="FluentFillerApi"/>
         /// </returns>
         public FluentFillerApi<TNewType> SetupFor<TNewType>(bool useDefaultSettings = false) where TNewType : class
         {
